bfb71fd924952f25e7390eb7f3374c27a6b69fb9
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-03-27 09:40:56 +0300

    Collection load method implemented

--- src/Collection/index.ts
+++ src/Collection/index.ts
@@ -1,6 +1,6 @@
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
-
+import { generate as generateId } from "shortid";
 import { IEntity } from "..";
 
 export class Collection<T extends IEntity> {
@@ -9,5 +9,14 @@ export class Collection<T extends IEntity> {
 
     constructor(dir: string, private name: string) {
         this.file = path.join(dir, name + ".json");
+        this.load();
+    }
+
+    private load() {
+        if (existsSync(this.file)) {
+            this.entities = JSON.parse(
+                readFileSync(this.file, { encoding: "utf8" })
+            );
+        }
     }
 }
