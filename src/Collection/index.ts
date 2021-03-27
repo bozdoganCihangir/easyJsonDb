d36c6bde317c490291f09bf2eaa42752e8753b10
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-03-27 09:46:51 +0300

    collection CRUD methods created

--- src/Collection/index.ts
+++ src/Collection/index.ts
@@ -11,6 +11,40 @@ export class Collection<T extends IEntity> {
         this.file = path.join(dir, name + ".json");
         this.load();
     }
+    create(obj: object): string {
+        const entity = { id: generateId(), ...obj } as T;
+        this.entities.push(entity);
+        this.save();
+        return entity.id;
+    }
+
+    delete(id: string): void {
+        const index = this.findIndex(id);
+        this.entities.splice(index, 1);
+        this.save();
+    }
+
+    get(id: string) {
+        return this.entities.find((item) => item.id === id);
+    }
+
+    list(): T[] {
+        return this.entities;
+    }
+
+    update(entity: T): void {
+        const index = this.findIndex(entity.id);
+        this.entities[index] = entity;
+        this.save();
+    }
+
+    private findIndex(id: string) {
+        const index = this.entities.findIndex((current) => current.id === id);
+        if (index === -1) {
+            throw new Error(`No ${this.name} found with id "${id}"`);
+        }
+        return index;
+    }
 
     private load() {
         if (existsSync(this.file)) {
@@ -18,5 +52,11 @@ export class Collection<T extends IEntity> {
                 readFileSync(this.file, { encoding: "utf8" })
             );
         }
+    }
+
+    private save() {
+        writeFileSync(this.file, JSON.stringify(this.entities, null, 2), {
+            encoding: "utf8",
+        });
     }
 }
